234572afe2cd40bd5a69e5fef562e289e806705a
park-d <54589582+park-d@users.noreply.github.com>
2022-12-14 13:17:40 -0600

    learning about path converters

--- challenges/views.py
+++ challenges/views.py
@@ -4,7 +4,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
-def monthly_challenge(request, month):
+def monthly_challenge_num(request, month):
+    challenge_text = None
+    if month == 1:
+        challenge_text = "january goal!"
+    elif month == 2:
+        challenge_text = "february goal!"
+    elif month == 3:
+        challenge_text = "march goal!"
+    else:
+        return HttpResponseNotFound("This month isn't supported yet.")
+    return HttpResponse(challenge_text)
+
+def monthly_challenge_str(request, month):
     challenge_text = None
     if month == 'january':
         challenge_text = "january goal!"
