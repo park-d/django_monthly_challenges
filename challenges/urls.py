d426ddfe64010c92437a0f31cf9ea0cf8e26ac7e
dnparkison <devan.parkison@gmail.com>
2023-01-27 14:48:07 -0600

    adding partials with include tag

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index), #/challenges/
+    path("", views.index, name="challenges-list"), #/challenges/
     path("<int:month>", views.monthly_challenge_num),
     path("<str:month>", views.monthly_challenge_str, name = "month-challenge")
 ]
